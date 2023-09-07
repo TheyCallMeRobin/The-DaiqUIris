2e2648336c0382e439cda1cbe1373810b56a3994
zDuncan01 <zachary.duncan@selu.edu>
2023-09-07 16:14:13 -0500

    Created and began a MainLandingPage.ui file

--- example.py
+++ example.py
@@ -3,6 +3,6 @@ from PyQt6 import QtWidgets, uic
 
 app = QtWidgets.QApplication(sys.argv)
 
-window = uic.loadUi("MainWindow.ui")
+window = uic.loadUi("MainLandingPage.ui")
 window.show()
 app.exec()
