4b191e1875fb43d448a3c0fd63076428876a2ee8
zDuncan01 <zachary.duncan@selu.edu>
2023-09-15 20:26:55 -0500

    Added sidebar functionality, still needs work

--- Front end/src/components/update/Modal/header.tsx
+++ Front end/src/components/update/Modal/header.tsx
@@ -11,8 +11,8 @@ const Header = () => {
   return (
     <header className={`header ${menuOpen ? 'menu-open' : ''}`}>
       <div className="container">
-        <div className="hamburger-menu" onClick={toggleMenu}>
-          <div className="bar"></div>
+         <div className="hamburger-menu" onClick={toggleMenu}> {/*<- This will be used after the onClick funtion to hide the header modal*/}
+          <div className="bar"></div>                           {/*from view in order to show a side bar Modal*/}
           <div className="bar"></div>
           <div className="bar"></div>
         </div>
@@ -20,12 +20,18 @@ const Header = () => {
           <img src={DaiqUIriLogo} alt="Logo" />
         </div>
       </div>
+      {menuOpen && (
+        <div className="side-menu">
+          <div className="menu-item">
+            <span>1</span>
+          </div>
+          <div className="menu-item">
+            <span>2</span>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
 
 export default Header;
-function setMenuOpen(arg0: boolean) {
-  throw new Error('Function not implemented.');
-}
-
